4bbd16af562e43b5e2cb30d9615d591c2c975850
HeavyMetal77 <ttm2006@ukr.net>
2020-01-22 20:38:53 +0200

    add Configuration, Pagination and Sorting

--- src/main/java/ua/tarastom/crudemployee/dao/EmployeeRepository.java
+++ src/main/java/ua/tarastom/crudemployee/dao/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package ua.tarastom.crudemployee.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ua.tarastom.crudemployee.entity.Employee;
 
+//optinal, give another path - http://localhost:8080/magic-api/members
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //Var.3 using REST API with Spring Data JPA Repository
 }
